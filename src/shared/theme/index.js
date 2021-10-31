239179f7e669bdd398d25503e5ecd7635b15ffd4
Andrew Bubnov <bubnov@ujin.tech>
2021-11-01 01:46:13 +0500

    custom primary theme color

--- src/shared/theme/index.js
+++ src/shared/theme/index.js
@@ -1,3 +1,12 @@
 import { createTheme } from '@mui/material/styles';
 
-export const theme = createTheme();
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#169889',
+      light: '#48ddcc',
+      dark: '#116c62',
+      contrastText: '#fff',
+    },
+  }
+});
