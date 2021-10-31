750dc1a14c089f5d188d3d24a960625c67c1fce3
Andrew Bubnov <bubnov@ujin.tech>
2021-10-31 18:46:18 +0500

    remove effector router

--- src/features/app/view/index.js
+++ src/features/app/view/index.js
@@ -1,9 +1,11 @@
+import { RouteLink, RouterView } from "effector-routing/dist/react"
 import { AppGate } from '../model'
 
 export function App() {
   return (
     <>
       <AppGate />
+      <RouterView />
       <div>app</div>
     </>
   )
